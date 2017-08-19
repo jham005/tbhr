3f98e51fc4ca5b68d559e3db28cbe141a9173004
jham005 <jham005@gmail.com>
2017-08-19 13:35:50 +0100

    Recaptcha server side validation

--- sendMessage.php
+++ sendMessage.php
@@ -1,10 +1,24 @@
 <?php
-if (empty($_POST['name']) || empty($_POST['email']) || empty($_REQUEST['message'])) {
+if (empty($_POST['name']) || empty($_POST['email']) || empty($_REQUEST['message']) || empty($_REQUEST['g-recaptcha-response'])) {
   header('Location: index.html#fail');
   exit(1);
 }
 
-$message = "\n-- " . date(DATE_RFC2822) . "\nA message from $_POST[name] <$_POST[email]>:\n$_POST[message]\n";
+$response =
+  @file_get_contents(
+    'https://www.google.com/recaptcha/api/siteverify',
+    false,
+    stream_context_create(
+      array('http' => array('header' => "Content-type: application/x-www-form-urlencoded\r\n",
+			    'method'  => 'POST',
+			    'content' => http_build_query(array('secret'=> '6Lcsay0UAAAAABNy3lK5hpXcXgc0OxsXP_GsagDa',
+								'response' => $_REQUEST['g-recaptcha-response'],
+								'remoteip' => $_SERVER['REMOTE_ADDR']))))));
+
+$message =
+  "\n-- "
+  . date(DATE_RFC2822)
+  . "\nA message from $_POST[name] <$_POST[email]>:\n$_POST[message]\n\nRecaptcha: $response\nReferer: $_SERVER[HTTP_REFERER]\nIP: $_SERVER[REMOTE_ADDR]\nUser agent: $_SERVER[HTTP_USER_AGENT]";
 
 file_put_contents('./messages.txt', $message, FILE_APPEND | LOCK_EX);
 
